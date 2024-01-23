c203bc63582cb4c38dfbe4c3c520249756d57eaf
haidar <haida.hs@gmail.com>
2024-01-23 10:27:30 +0700

    update

--- src/script.js
+++ src/script.js
@@ -2,11 +2,19 @@
 window.onscroll = function(){
     const header = document.querySelector('header');
     const navbarFixed = header.offsetTop;
+    const toTop = document.querySelector('#to-top')
 
     if (window.pageYOffset > navbarFixed){
-        header.classList.add('navbar-fixed'); 
+        header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden') ;
+
+
+       
     }else{
         header.classList.remove('navbar-fixed');
+        toTop.classList.add('hidden');
+
+
     }
 }
 
@@ -18,3 +26,34 @@ humberger.addEventListener('click',function(){
     humberger.classList.toggle('active');
     navMenu.classList.toggle('hidden');
 });
+
+// klik dimana pun
+ window.addEventListener('click', function(a){
+   if(a.target !=humberger && a.target !=navMenu){
+    humberger.classList.remove('active');
+    navMenu.classList.add('hidden');
+
+   }
+ });
+
+
+//  dark mode
+// darkmode
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+darkToggle.addEventListener('click', function () {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    };
+});
+
+// pindah tombol 
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
